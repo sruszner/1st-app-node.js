4a475c594f28088d857f25f5424476f7bbce6c97
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-07-04 15:47:32 -0300

    DB Local y termino funciones generales

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const hbs = require('hbs');
 const mysql = require('mysql2');
 const path = require('path');
 const nodemailer = require('nodemailer');
-
+const session = require('express-session');
+const { request } = require('http');
 
 // Conectamos la app a una Base de Datos
 
@@ -25,6 +26,12 @@ conexion.connect(function (error) {
     if (error) throw error;
     console.log("Conexion a la DB exitosa");
 })
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 /*     const conectar = async({
     await conexion.connect((error) => {
@@ -68,7 +75,7 @@ app.get('/pricing', (req, res) => {
 })
 
 app.get('/administrator', (req, res) => {
-    let sql = "SELECT * FROM USUARIO";
+    let sql = "SELECT * FROM FORMULARIO";
     let query = conexion.query(sql, (err, results) => {
         if (err) throw err;
         res.render('administrator', { tabla1: 'Contact List', results })
@@ -80,7 +87,7 @@ app.post('/administrator', (req, res) => {
     console.log(req.body.email);
     console.log(req.body.id);
     //res.send("Actualizamos los datos");
-    let sql = "UPDATE USUARIO SET firstName='" + req.body.firstname + "', email='" + req.body.email + "' WHERE idGenero=" + req.body.id;
+    let sql = "UPDATE FORMULARIO SET firstName='" + req.body.firstname + "', email='" + req.body.email + "', plan='" + req.body.plan + "' WHERE id=" + req.body.id;
     let query = conexion.query(sql, (err, results) => {
         if (err) throw err;
         res.redirect('/administrator')
@@ -91,7 +98,7 @@ app.post('/delete', (req, res) => {
 
     console.log(req.body.id);
     //res.send("Eliminamos los datos");
-    let sql = "DELETE FROM USUARIO WHERE idGenero=" + req.body.id;
+    let sql = "DELETE FROM FORMULARIO WHERE id=" + req.body.id;
     let query = conexion.query(sql, (err, results) => {
         if (err) throw err;
         res.redirect('/administrator')
@@ -107,7 +114,7 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/login', (req, res) => {
+/* app.post('/login', (req, res) => {
     console.log(req.body);
     //res.send({respuesta: "tus datos son correctos"})
     const { usuario, password, remember } = req.body;
@@ -116,6 +123,41 @@ app.post('/login', (req, res) => {
     console.log(remember);
     res.send({ respuesta: "tus datos son correctos" })
 })
+ */
+
+
+// Autenticacion LOGIN
+
+
+app.post('/auth', function (request, response) {
+    // Capture the input fields
+    let usuario = request.body.usuario;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (usuario && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        conexion.query('SELECT * FROM REGISTRO WHERE email = ? AND pass = ?', [usuario, password], function (error, results, fields) {
+            // If there is an issue with the query, output the error
+            if (error) throw error;
+            // If the account exists
+            if (results.length > 0) {
+                // Authenticate the user
+                request.session.loggedin = true;
+                request.session.usuario = usuario;
+                // Redirect to home page
+                console.log('Sesión iniciada correctamente');
+                response.redirect('/');
+            } else {
+                console.log('Datos incorrectos');
+                response.redirect('login');
+            }
+            response.end();
+        });
+    } else {
+        response.redirect('login');
+        response.end();
+    }
+});
 
 app.get('/forgot', (req, res) => {
     res.render('forgot')
@@ -126,7 +168,7 @@ app.post('/forgot', (req, res) => {
     //res.send({respuesta: "tus datos son correctos"})
     const { emailForgot } = req.body;
     console.log(emailForgot);
-    res.send({ respuesta: "Revisa tu email y sigue los pasos para reestablecer la contraseña" })
+    res.send({ respuesta: "Check your email and follow the steps to reset your password" })
 })
 
 app.get('/reset-pass', (req, res) => {
@@ -139,7 +181,7 @@ app.post('/reset-pass', (req, res) => {
     const { password, logout_devices } = req.body;
     console.log(password);
     console.log(logout_devices);
-    res.send({ respuesta: "Tu constraseña se reestablecio correctamente" })
+    res.send({ respuesta: "Your password has been successfully reset" })
 })
 
 
@@ -149,20 +191,56 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-    let { emailRegister, nameRegister, passwordRegister } = req.body;
-    console.log(emailRegister);
-    console.log(nameRegister);
-    console.log(passwordRegister);
+    let { email, firstName, pass } = req.body;
+    console.log(email);
+    console.log(firstName);
+    console.log(pass);
 
     let register = {
-        email: emailRegister,
-        name: nameRegister,
-        password: passwordRegister
+        email: email,
+        firstName: firstName,
+        pass: pass,
+        plan: "1"
     }
 
-    let sql = "INSERT INTO LOG SET ?";
+    let sql = "INSERT INTO REGISTRO SET ?";
     let query = conexion.query(sql, register, (err, results) => {
         if (err) throw err;
+
+        let transporter = nodemailer.createTransport({
+            //service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'spafrancorchampsapp@gmail.com',
+                pass: 'yqcocvlwiksrgvsy'
+            }
+        });
+
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Server is ready to take our messages");
+            }
+        });
+
+
+        var mailOptions = {
+            from: 'spafrancorchapsapp@gmail.com',
+            to: email,
+            subject: 'SPA Circuit - Register',
+            html: ('<h3>Thank you so much for your registration, ' + firstName + '</h3>' + ' <p>These are your login details</p> Email: ' + email + '<br>Password: ' + pass)
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
         res.render('login')
     });
 })
@@ -185,11 +263,12 @@ app.post('/form-received', (req, res) => {
     console.log(req.body.country);
     console.log(req.body.state);
     console.log(req.body.zip);
-    console.log(req.body.textarea);
+    console.log(req.body.plan);
+    console.log(req.body.message);
     console.log(req.body.dataStorage);
     console.log(req.body.beContacted);
     console.log(req.body.metOn);
-    const { firstName, lastName, username, email, address, address2, country, state, zip, textarea, dataStorage, beContacted, metOn } = req.body;
+    const { firstName, lastName, username, email, address, address2, country, state, zip, plan, message, dataStorage, beContacted, metOn } = req.body;
     //conectar();
 
     // no le paso el text area a data porque no lo declare en la Database
@@ -204,14 +283,51 @@ app.post('/form-received', (req, res) => {
         country: country,
         state: state,
         zip: zip,
+        plan: plan,
+        message: message,
         dataStorage: dataStorage,
         beContacted: beContacted,
         metOn: metOn
     }
 
-    let sql = "INSERT INTO USUARIO SET ?";
+    let sql = "INSERT INTO FORMULARIO SET ?";
     let query = conexion.query(sql, data, (err, results) => {
         if (err) throw err;
+
+        let transporter = nodemailer.createTransport({
+            //service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'spafrancorchampsapp@gmail.com',
+                pass: 'yqcocvlwiksrgvsy'
+            }
+        });
+
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Server is ready to take our messages");
+            }
+        });
+
+
+        var mailOptions = {
+            from: 'spafrancorchapsapp@gmail.com',
+            to: 'ruszner.sebastian@gmail.com',
+            subject: 'SPA Circuit - NEW CONTACT FORM',
+            html: ('<h3>Please check the latest contact form</h3>' + ' <p>These are the contact details</p> Name: ' + firstName + '<br>Last name: ' + lastName + '<br>email: ' + email + '<br>Plan Code: ' + plan + '<br>Message: ' + message)
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
         res.render('form-received');
     })
 })
@@ -220,9 +336,16 @@ app.post('/form-received', (req, res) => {
 
 app.post('/subscribed', (req, res) => {
     console.log(req.body);
-    const { emailNewsletter } = req.body;
+    const { email } = req.body;
+    let data = {
+        email: email,
+        plan: "1"
+    }
     console.log('subscribed');
-
+    let sql = "INSERT INTO NEWSLETTER SET ?";
+    let query = conexion.query(sql, data, (err, results) => {
+        if (err) throw err;
+    })
     let transporter = nodemailer.createTransport({
         //service: 'gmail',
         host: "smtp.gmail.com",
@@ -245,7 +368,7 @@ app.post('/subscribed', (req, res) => {
 
     var mailOptions = {
         from: 'spafrancorchapsapp@gmail.com',
-        to: emailNewsletter,
+        to: email,
         subject: 'SPA Circuit - Subscribed',
         html: '<h3>Thank you so much for your subscription,</h3> <p>it means a lot to us. We really appreciate you taking a moment of your time today.</p>'
     };
